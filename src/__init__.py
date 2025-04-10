819ee6bb945ae3b44a13213e2d934c1514647937
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-47-4.harvard.edu>
2025-04-10 12:42:54 -0400

    aggregate directory and update init

--- src/__init__.py
+++ src/__init__.py
@@ -12,9 +12,9 @@ from .lfc3d.prioritize_sequence_plot import plot_screendata_sequence
 from .lfc3d.randomize_sequence import randomize_sequence
 
 from .lfc3d.calculate_lfc3d import calculate_lfc3d
-from .lfc3d.metaaggregate import average_split_meta, bin_meta, znorm_meta
-from .lfc3d.nonaggregate import average_split_score, bin_score, znorm_score
-from .lfc3d.aggregate_plot import average_split_bin_plots
+from .aggregate.metaaggregate import average_split_meta, bin_meta, znorm_meta
+from .aggregate.nonaggregate import average_split_score, bin_score, znorm_score
+from .aggregate.aggregate_plot import average_split_bin_plots
 
 from .lfc3d.characterization import enrichment_test
 from .lfc3d.characterization_plot import plot_enrichment_test, lfc_lfc3d_scatter, pLDDT_RSA_scatter, hits_feature_barplot
