31409cb648fc30103c1f891fb56f2e6c431c3769
xycalvin <68132984+xycalvin@users.noreply.github.com>
2025-07-22 18:03:26 -0400

    Update conservation.py

--- beclust3d/lfc3d/conservation.py
+++ beclust3d/lfc3d/conservation.py
@@ -1,6 +1,6 @@
 """
 File: conservation.py
-Author: Calvin XiaoYang Hu, Surya Kiran Mani, Sumaiya Iqbal
+Author: Calvin XiaoYang Hu, Yoochan Myung, Surya Kiran Mani, Sumaiya Iqbal
 Date: 2024-06-25
 Description: Translated from Notebook 2
 """
