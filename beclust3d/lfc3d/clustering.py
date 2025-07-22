e0dee83dc3593900f81b7c1caf2e1cb04341de3a
xycalvin <68132984+xycalvin@users.noreply.github.com>
2025-07-22 18:03:42 -0400

    Update clustering.py

--- beclust3d/lfc3d/clustering.py
+++ beclust3d/lfc3d/clustering.py
@@ -1,6 +1,6 @@
 """
 File: clustering.py
-Author: Calvin XiaoYang Hu, Surya Kiran Mani, Sumaiya Iqbal
+Author: Calvin XiaoYang Hu, Yoochan Myung, Surya Kiran Mani, Sumaiya Iqbal
 Date: 2024-06-18
 Description: 
 """
