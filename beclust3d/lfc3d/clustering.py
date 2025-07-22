160e448db4584257d1d7373a708ef7f78dc0c595
xycalvin <68132984+xycalvin@users.noreply.github.com>
2025-07-22 17:54:39 -0400

    Update clustering.py minor changes

--- beclust3d/lfc3d/clustering.py
+++ beclust3d/lfc3d/clustering.py
@@ -15,14 +15,17 @@ from sklearn.cluster import AgglomerativeClustering
 
 
 def clustering(
-        df_struc, df_pvals, 
-        workdir, input_gene, 
-        max_distances=25, 
-        psig_columns=[f'SUM_LFC3D_neg_05_psig', f'SUM_LFC3D_pos_05_psig'], # CATEGORICAL NOT QUANTITATIVE #
-        pthr_cutoffs=['p<0.05', 'p<0.05'], 
-        screen_name='Meta', score_type='LFC3D', 
-        merge_cols=['unipos', 'chain'], 
-        clustering_kwargs = {"n_clusters": None, "metric": "euclidean", "linkage": "single"}, 
+    df_struc, 
+    df_pvals, 
+    workdir, 
+    input_gene, 
+    max_distances=25, 
+    psig_columns=[f'SUM_LFC3D_neg_05_psig', f'SUM_LFC3D_pos_05_psig'], # CATEGORICAL NOT QUANTITATIVE #
+    pthr_cutoffs=['p<0.05', 'p<0.05'], 
+    screen_name='Meta', 
+    score_type='LFC3D', 
+    merge_cols=['unipos', 'chain'], 
+    clustering_kwargs={"n_clusters": None, "metric": "euclidean", "linkage": "single"}, 
 ): 
     """
     Performs spatial-based agglomerative clustering of residues based on significance, 
@@ -30,7 +33,7 @@ def clustering(
 
     Parameters
     ----------
-    df_str_cons : pd.DataFrame
+    df_struc : pd.DataFrame
         DataFrame containing structural data for residues. 
         Must include ['unipos', 'unires', 'chain', 'x_coord', 'y_coord', 'z_coord'].
 
@@ -44,6 +47,9 @@ def clustering(
     input_gene : str
         Name of the gene being processed. 
 
+    max_distances : int, optional (default=25)
+        Maximum radius (in Angstroms) to consider for clustering. Clustering is repeated at every integer from 3 to `max_distances`.
+        
     psig_columns : list of str, optional
         List of column names in `df_pvals` indicating categorical significance labels 
         (e.g., 'p<0.05' for significant residues to cluster).
@@ -57,9 +63,6 @@ def clustering(
 
     score_type : str, optional (default='LFC3D')
         Label for the type of mutation score analyzed (e.g., 'LFC3D', 'LFC', etc.).
-
-    max_distances : int, optional (default=25)
-        Maximum radius (in Angstroms) to consider for clustering. Clustering is repeated at every integer from 4 to `max_distances`.
 
     merge_cols : list of str, optional (default=['unipos', 'chain'])
         Columns used to merge clustering results back into the main DataFrame.
